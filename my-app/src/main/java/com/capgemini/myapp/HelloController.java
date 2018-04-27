4c2f8a7a176a87a51affba769e7d9cce2c3cf344
msisodia <manisha.sisodia@capgemini.com>
2018-04-27 13:37:26 +0530

    No comment

--- my-app/src/main/java/com/capgemini/myapp/HelloController.java
+++ my-app/src/main/java/com/capgemini/myapp/HelloController.java
@@ -9,8 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HelloController {
 
 	@RequestMapping("/hello")
-	//Second Polling Test
-	//Third Polling Test
+	//No comment
 	public String hello(Model model,
 			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
 		model.addAttribute("name", name);
