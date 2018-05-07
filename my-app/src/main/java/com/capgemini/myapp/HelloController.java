d641259796e0a6e55775ee793807593957adb16b
msisodia <manisha.sisodia@capgemini.com>
2018-05-07 10:19:55 +0530

    AWS Linux Machine check

--- my-app/src/main/java/com/capgemini/myapp/HelloController.java
+++ my-app/src/main/java/com/capgemini/myapp/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
-
+//AWS linux machine is running....
 	@RequestMapping("/hello")
 	public String hello(Model model,
 			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
